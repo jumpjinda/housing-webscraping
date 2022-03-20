494b9a77edb8d8aff716c4d900089c5bb76099b4
jumpjinda <jumpjinda@gmail.com>
2022-03-20 21:56:48 +0700

    last real final

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from scrape_zillow_ca_csv import *
+from scrape_zillow_to_csv_and_json import *
 from load_csv_to_postgres import *
+from load_json_to_mongodb import *
 
 state_list = ['al', 'ak', 'ar', 'az', 'ca', 'co', 'ct', 'de', 'dc', 'fl', 'ga', 'hi', 'id', 'il', 'in_', 'ia', 'ks', 'ky', 'la', 'me', 'md', 'ma', 'mi' ,'ms', 'mo', 'mt', 'ne', 'nv', 'nh', 'nj', 'nm', 'ny', 'nc', 'nd' , 'oh', 'ok', 'or_', 'pa', 'ri', 'sc', 'sd', 'tn', 'ut', 'vt', 'va', 'wa', 'wv', 'wi', 'wy']
 
-for item in state_list:
-    zillow_housing_scrape(item, 21)
+for state in state_list:
+    zillow_housing_scrape(state, 21)
     
-for item in state_list:
-    load_csv_to_postgres('housing', item)
+for state in state_list:
+    load_csv_to_postgres('housing', state)
+    
+for state in state_list:
+    load_json_to_mongodb(state)
